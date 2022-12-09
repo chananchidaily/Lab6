c8a5a7ef8ed1a29fd946da6771f8ae60aacee027
tonkhaooo <Chananchida_tha@cmu.ac.th>
2022-12-09 16:59:39 +0700

    lab6_2.cpp completed

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
+
+double deg2rad(double deg);
+double rad2deg(double rad);
+double findXComponent(double length1,double rad1 ,double length2,double rad2 );
+double findYComponent(double length1,double rad1 ,double length2,double rad2 );
+double pythagoras(double X_all , double Y_all);
+void showResult(double resule , double reg);
 
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
@@ -19,6 +26,40 @@ int main(){
     ycomp = findYComponent(l1,l2,a1,a2);
     result_vec_length = pythagoras(xcomp,ycomp);
     result_vec_direction = rad2deg(atan2(ycomp,xcomp)); 
-
     showResult(result_vec_length,result_vec_direction);
+
+    
+    
+}
+
+double deg2rad(double deg){
+    double rad = deg*M_PI /180 ;
+    return rad ;
+}
+double rad2deg(double rad){
+    double deg = rad*180/M_PI;
+    return deg ;
+}
+double findXComponent(double l1,double l2 ,double r1,double r2 ){
+    double Xcompo1 = l1*cos(r1);
+    double Xcompo2 = l2*cos(r2);
+    double Xcompo_all = Xcompo1+Xcompo2;
+    return Xcompo_all;
+}
+double findYComponent(double l1,double l2 ,double r1,double r2 ){
+    double Ycompo1 = l1*sin(r1);
+    double Ycompo2 = l2*sin(r2);
+    double Ycompo_all = Ycompo1+Ycompo2;
+    return Ycompo_all;
+}
+double pythagoras(double X_all , double Y_all){
+    double Resule = sqrt(pow(X_all,2)+pow(Y_all,2));
+    return Resule;
+
+}
+void showResult(double resule , double reg){
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" << endl;
+    cout << "Length of the resultant vector = " << resule <<endl;
+    cout << "Direction of the resultant vector (deg) = "<< reg << endl;
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%";
 }
